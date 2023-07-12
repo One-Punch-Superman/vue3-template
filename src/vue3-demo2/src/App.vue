ac22a00a26ce726f85fe0476055b091754d5869d
One-Punch-Superman <764213669@qq.com>
2023-07-12 23:16:07 +0800

    init

---
+++ src/vue3-demo2/src/App.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    11
+    <router-view />
+  </div>
+</template>
---
+++ src/vue3-demo2/src/views/AboutView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="">{{ str }}</div>
+</template>
+
+<script setup>
+import { ref, onMounted } from "vue";
+
+const str = ref("about");
+onMounted(() => {});
+</script>
+
+<style scoped></style>
