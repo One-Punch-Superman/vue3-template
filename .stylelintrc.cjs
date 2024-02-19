762f9071f4394099d46cafef51571c1a4f4d1623
One-Punch-Superman <764213669@qq.com>
2024-02-20 00:27:59 +0800

    init

--- .stylelintrc.cjs
+++ .stylelintrc.cjs
@@ -1,18 +1,18 @@
 // @see: https://stylelint.io
 
 module.exports = {
-  /* 继承某些已有的规则 */
   extends: [
     'stylelint-config-standard',
-    'stylelint-config-html/vue',
     'stylelint-config-standard-scss',
     'stylelint-config-recommended-vue/scss',
-    'stylelint-config-prettier'
+    'stylelint-config-html/vue',
+    'stylelint-config-recess-order'
   ],
-  /* 使用排序插件 */
-  plugins: ['stylelint-order'],
   overrides: [
-    // 扫描 .vue/html 文件中的<style>标签内的样式
+    {
+      files: ['**/*.{scss,css}'],
+      customSyntax: 'postcss-scss'
+    },
     {
       files: ['**/*.{vue,html}'],
       customSyntax: 'postcss-html'
